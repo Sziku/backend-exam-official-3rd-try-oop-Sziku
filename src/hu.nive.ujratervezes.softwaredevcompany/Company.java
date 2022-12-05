e361fc149116df69fa957f6702dc1a80604ddca7
Sziku <105989158+Sziku@users.noreply.github.com>
2022-12-05 10:16:36 +0100

    add Company.java with filed and method

--- src/hu.nive.ujratervezes.softwaredevcompany/Company.java
+++ src/hu.nive.ujratervezes.softwaredevcompany/Company.java
@@ -1,2 +1,18 @@
+import java.util.List;
+
 public class Company {
+    private List<Worker> workerList;
+
+    public Company(List<Worker> workerList) {
+        this.workerList = workerList;
+    }
+
+    public int companyExperience(){
+        int sumExp = 0;
+        for(Worker worker : workerList){
+            sumExp += worker.getExperience();
+        }
+
+        return sumExp;
+    }
 }
